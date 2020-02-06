4c92853c9b67d874d70b177636be64c2c1564e77
andrzejbajuk79 <ptasiek3003@gmail.com>
2020-02-06 15:46:28 +0100

    init

--- client/src/redux/actions/action.js
+++ client/src/redux/actions/action.js
@@ -1,25 +1,23 @@
-import axios from 'axios';
-import { GET_BOOKS } from './../constants/contants';
+import axios from "axios";
+import { GET_BOOKS } from "./../constants/contants";
 
-
-export function getBooks(
- limit = 10,
- start = 0,
- order = 'asc',
- list = ''
-) {
- const request = axios.get(`
+export function getBooks(limit = 10, start = 0, order = "asc", list = "") {
+ const request = axios
+  .get(
+   `
  /api/books?limit=${limit}&skip=${start}&order=${order}`
- ).then(res => {
-  if (list) {
-   return [...list, ...res.data]
-  } else {
-   return res.data
-  }
- }
- )
+  )
+  .then(res => {
+   if (list) {
+    return [...list, ...res.data];
+   } else {
+    return res.data;
+   }
+  });
  return {
   type: GET_BOOKS,
   payload: request
- }
+ };
 }
+
+export function getBookWithReviewer() {}
